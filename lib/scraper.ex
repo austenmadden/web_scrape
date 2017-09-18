fe115dacaad742c73d87708542539792ff67c657
Austen Madden <amadden@covermymeds.com>
2017-09-18 17:07:39 -0400

    WIP: Start collecting content from title tags

--- lib/scraper.ex
+++ lib/scraper.ex
@@ -1,8 +1,15 @@
+require IEx;
+
 defmodule Scraper do
   def scrape_web_page(url) when is_bitstring(url) do
-    HTTPoison.get!("#{url}")
-    |> IO.inspect
+    case HTTPoison.get(url, [], []) do
+      {:ok, %{body: body, headers: headers, status_code: 200}} ->
+        %{"title" => title} = Regex.named_captures(~r/<title>(?<title>.+)<\/title>/, body)
+        IO.puts(title)
+      {:error, %HTTPoison.Error{reason: reason}} -> IO.inspect(reason)
+    end
   end
+
 
   def scrape_web_page(_), do: IO.puts "Error, check input"
 
