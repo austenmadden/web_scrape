833c3f6a9967f89dd9e79f818871e1220c146807
Austen Madden <amadden@covermymeds.com>
2017-09-19 14:16:43 -0400

    Working crawler

--- lib/scraper.ex
+++ lib/scraper.ex
@@ -1,21 +1,37 @@
 defmodule Scraper do
   def scrape_web_page(url) when is_bitstring(url) do
-    case HTTPoison.get(url, [], []) do
-      {:ok, %{body: body, headers: headers, status_code: 200}} ->
-        body
-        |> Floki.find("a")
-        |> Floki.attribute("href")
-        |> Enum.filter(&Regex.match?(~r/http(s?):\/\/www./, &1))
-        |> Enum.map(&HTTPoison.get(&1))
-        |> Enum.map(&(elem(&1, 1).body))
-        |> Enum.map(&Regex.run(~r/<title>(.+)<\/title>/, &1))
-        |> Enum.filter(&(&1 != nil))
-        |> Enum.map(&List.last(&1))
-      {:error, %HTTPoison.Error{reason: reason}} -> IO.inspect(reason)
-    end
+    crawl(url)
   end
 
   def scrape_web_page(_), do: IO.puts "Error, check input"
 
   def scrape_web_page, do: IO.puts "Error, requires url"
+
+  defp crawl(url, titles \\ %{}) when is_bitstring(url) do
+    case HTTPoison.get(url, [], []) do
+      {:ok, %{body: body, headers: _, status_code: 200}} ->
+        title = body
+        |> Floki.find("title")
+        |> List.last
+        |> elem(2)
+        |> List.last
+
+        if (titles[url] == nil) do
+          titles = Map.put(titles, url, title)
+          IO.inspect("#{url}, #{title}")
+
+          titles = body
+          |> Floki.find("a")
+          |> Floki.attribute("href")
+          |> Enum.filter(&Regex.match?(~r/http(s?):\/\//, &1))
+          |> Enum.reduce(titles, fn(found_url, acc) -> Map.merge(acc, crawl(found_url, acc)) end)
+
+          titles
+        else
+          titles
+        end
+      {:ok, _} -> titles
+      {:error, %HTTPoison.Error{reason: _}} -> titles
+    end
+  end
 end
