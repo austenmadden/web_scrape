95da33723f7a28e1c97ab1fd979161ab4df3207d
Austen Madden <amadden@covermymeds.com>
2017-09-18 22:19:40 -0400

    Basic web crawling, parse titles into list

--- lib/scraper.ex
+++ lib/scraper.ex
@@ -1,15 +1,19 @@
-require IEx;
-
 defmodule Scraper do
   def scrape_web_page(url) when is_bitstring(url) do
     case HTTPoison.get(url, [], []) do
       {:ok, %{body: body, headers: headers, status_code: 200}} ->
-        %{"title" => title} = Regex.named_captures(~r/<title>(?<title>.+)<\/title>/, body)
-        IO.puts(title)
+        body
+        |> Floki.find("a")
+        |> Floki.attribute("href")
+        |> Enum.filter(&Regex.match?(~r/http(s?):\/\/www./, &1))
+        |> Enum.map(&HTTPoison.get(&1))
+        |> Enum.map(&(elem(&1, 1).body))
+        |> Enum.map(&Regex.run(~r/<title>(.+)<\/title>/, &1))
+        |> Enum.filter(&(&1 != nil))
+        |> Enum.map(&List.last(&1))
       {:error, %HTTPoison.Error{reason: reason}} -> IO.inspect(reason)
     end
   end
-
 
   def scrape_web_page(_), do: IO.puts "Error, check input"
 
